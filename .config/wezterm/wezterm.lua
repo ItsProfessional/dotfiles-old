5dcd0ed749ee9531ed16315473a04324751d0a8e
ItsProfessional <63961221+ItsProfessional@users.noreply.github.com>
2023-06-01 22:59:54 +0530

    Wezterm: map ctrl+num to escape sequences so that tmux can bind them

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -19,6 +19,20 @@ config.color_scheme = 'Catppuccin Mocha'
 config.font = wezterm.font('JetBrainsMono Nerd Font', { weight = "Regular" })
 config.font_size = 11
 
+-- This is required in the wezterm config for the tmux config described here to work: https://unix.stackexchange.com/a/96936
+config.keys = {
+  { key = "0", mods = "CTRL", action = act.SendString '\x1b[27;5;48~' },
+  { key = "1", mods = "CTRL", action = act.SendString '\x1b[27;5;49~' },
+  { key = "2", mods = "CTRL", action = act.SendString '\x1b[27;5;50~' },
+  { key = "3", mods = "CTRL", action = act.SendString '\x1b[27;5;51~' },
+  { key = "4", mods = "CTRL", action = act.SendString '\x1b[27;5;52~' },
+  { key = "5", mods = "CTRL", action = act.SendString '\x1b[27;5;53~' },
+  { key = "6", mods = "CTRL", action = act.SendString '\x1b[27;5;54~' },
+  { key = "7", mods = "CTRL", action = act.SendString '\x1b[27;5;55~' },
+  { key = "8", mods = "CTRL", action = act.SendString '\x1b[27;5;56~' },
+  { key = "9", mods = "CTRL", action = act.SendString '\x1b[27;5;57~' },
+}
+
 config.mouse_bindings = {
   -- Change the default click behavior so that it only selects text and doesn't open hyperlinks
   {
